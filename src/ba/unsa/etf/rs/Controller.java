a37d148e5ee2d571bed5f6614be6c70ed00f00ee
gjovic1 <gjovic1@etf.unsa.ba>
2020-03-11 09:58:50 +0100

    Urađen zadatak 1.

--- src/ba/unsa/etf/rs/Controller.java
+++ src/ba/unsa/etf/rs/Controller.java
@@ -1,8 +1,16 @@
 package ba.unsa.etf.rs;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Controller {
+    public Button btnChg;
+    public Label labelica;
     public void Submit(ActionEvent actionEvent) {
+    }
+
+    public void changeText(ActionEvent actionEvent) {
+        labelica.setText("Hello World");
     }
 }
